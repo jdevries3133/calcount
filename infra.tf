3a02ea25c900511089c0fefaf9945278ddc9a8e3
Jack DeVries <jdevries3133@gmail.com>
2023-12-25 14:26:55 -0500

    Revert "feat: retry move to new domain"
    This reverts commit d3c1ee9bfa635138c995ff8ca09c56ae10d580f0.

--- infra.tf
+++ infra.tf
@@ -57,7 +57,7 @@ module "basic-deployment" {
 
   app_name  = "calcount"
   container = "jdevries3133/calcount:${data.external.git_describe.result.output}"
-  domain    = "beancount.bot"
+  domain    = "calcount.jackdevries.com"
 
   extra_env = {
     SESSION_SECRET = random_password.secret_key.result
