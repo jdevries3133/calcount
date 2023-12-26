d871261b4bab2c968a8a4e591b5572d38c431c56
Jack DeVries <jdevries3133@gmail.com>
2023-12-26 08:26:54 -0500

    feat: duplicate deployment on new domain

--- infra.tf
+++ infra.tf
@@ -49,8 +49,6 @@ data "external" "git_describe" {
   ]
 }
 
-// This infrastructure module is fairly tightly coupled to my homelab
-// Kubernetes cluster. YMMV
 module "basic-deployment" {
   source  = "jdevries3133/basic-deployment/kubernetes"
   version = "3.0.2"
@@ -58,6 +56,21 @@ module "basic-deployment" {
   app_name  = "calcount"
   container = "jdevries3133/calcount:${data.external.git_describe.result.output}"
   domain    = "calcount.jackdevries.com"
+
+  extra_env = {
+    SESSION_SECRET = random_password.secret_key.result
+    OPENAI_API_KEY = var.openai_api_key
+    STRIPE_API_KEY = var.stripe_api_key
+  }
+}
+
+module "basic-deployment-beancount" {
+  source  = "jdevries3133/basic-deployment/kubernetes"
+  version = "3.0.2"
+
+  app_name  = "beancount"
+  container = "jdevries3133/calcount:${data.external.git_describe.result.output}"
+  domain    = "beancount.bot"
 
   extra_env = {
     SESSION_SECRET = random_password.secret_key.result
