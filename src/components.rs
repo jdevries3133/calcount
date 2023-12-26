54e1dc887352031f17ba2bd98a204798ef6026b9
Jack DeVries <jdevries3133@gmail.com>
2023-12-26 08:58:23 -0500

    fix: add html lang attribute

--- src/components.rs
+++ src/components.rs
@@ -76,7 +76,7 @@ impl Component for Page<'_> {
         let htmx = Route::Htmx;
         format!(
             r#"
-            <html>
+            <html lang="en">
                 <head>
                     <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
                     <title>{title}</title>
