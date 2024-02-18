795cfba61a19508ed28ff2fc9bc515516e52d0f8
Jack DeVries <jdevries3133@gmail.com>
2024-02-18 09:32:47 -0500

    chore: remove dead code

--- src/db_ops.rs
+++ src/db_ops.rs
@@ -22,36 +22,14 @@ pub async fn create_pg_pool() -> Result<sqlx::Pool<sqlx::Postgres>> {
 }
 
 #[async_trait]
-pub trait DbModel<GetQuery, ListQuery>: Sync + Send {
+pub trait DbModel<GetQuery>: Sync + Send {
     /// Get exactly one object from the database, matching the query. WIll
     /// return an error variant if the item does not exist.
     async fn get(db: &PgPool, query: &GetQuery) -> Result<Self>
     where
         Self: Sized;
-    /// Get a set of objects from the database, matching the contents of the
-    /// list query type.
-    async fn list(db: &PgPool, query: &ListQuery) -> Result<Vec<Self>>
-    where
-        Self: Sized;
     /// Persist the object to the database
     async fn save(&self, db: &PgPool) -> Result<()>;
-    /// Delete the record from the databse, which could of course cascade
-    /// to related rows based on the rules in the database schema for this
-    /// table.
-    ///
-    /// Delete will consume `self`.
-    ///
-    /// Most `.save` methods are implemented using update queries, under the
-    /// assumption that the object already exists and we are just mutating it
-    /// and then calling `.save` to persist the mutation. Deletion, then,
-    /// would naturally invalidate these save queries.
-    ///
-    /// Additionally, a delete operation can trigger cascading deletion,
-    /// so the existing record will often change structurally after deletion,
-    /// because other rows around it will be deleted as well. The strategy
-    /// for recovering from deletion will vary based on the object type,
-    /// which is why the delete method consumes `self`.
-    async fn delete(self, _db: &PgPool) -> Result<()>;
 }
 
 pub enum UserIdentifer<'a> {
@@ -86,7 +64,7 @@ fn map_into_user(row: Qres) -> models::User {
 }
 
 #[async_trait]
-impl DbModel<GetUserQuery<'_>, ()> for models::User {
+impl DbModel<GetUserQuery<'_>> for models::User {
     async fn get(db: &PgPool, query: &GetUserQuery) -> Result<Self> {
         Ok(match query.identifier {
             UserIdentifer::Id(id) => {
@@ -127,9 +105,6 @@ impl DbModel<GetUserQuery<'_>, ()> for models::User {
             }
         })
     }
-    async fn list(_db: &PgPool, _query: &()) -> Result<Vec<Self>> {
-        todo!()
-    }
     async fn save(&self, db: &PgPool) -> Result<()> {
         query!(
             "update users set
@@ -148,8 +123,5 @@ impl DbModel<GetUserQuery<'_>, ()> for models::User {
         .execute(db)
         .await?;
         Ok(())
-    }
-    async fn delete(self, _db: &PgPool) -> Result<()> {
-        todo!();
     }
 }
